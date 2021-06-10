c17679fbdf3e7d3dba042b801d28474d89382520
BazzaDEV <bazarjani.kian@gmail.com>
2021-06-09 23:55:41 -0400

    Created task to push resume to Dropbox by replacing the existing PDF file

---
+++ .vscode/deploy-to-dropbox.cmd
@@ -0,0 +1,4 @@
+@echo off
+copy main.pdf resume.pdf
+copy resume.pdf "C:\Users\bazar\Dropbox\shared files"
+del resume.pdf
---
+++ .vscode/deploy-to-dropbox.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp main.pdf resume.pdf
+copy resume.pdf "C:\Users\bazar\Dropbox\shared files"
+del resume.pdf
